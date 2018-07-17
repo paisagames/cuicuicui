3390afbe265d43ff16959f32b735108e61fdaef4
juanregules <33490910+juanregules@users.noreply.github.com>
2018-07-16 23:27:08 -0700

    VELOCIMETRO Y ESTRELLAS
    -velocimetro corregido
    -balas mejoradas
    -ESTRELLAS cambiadas por los asteroides de carlos

---
+++ cui cui cui/Assets/Assets/codigos/copiarotacion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class copiarotacion : MonoBehaviour {
+
+	public Transform nave;
+	// Use this for initialization
+	void Start () {
+		transform.rotation=nave.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation=nave.rotation;
+		
+	}
+}
---
+++ cui cui cui/Assets/Assets/codigos/nivel2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nivel2 : MonoBehaviour {
+public MeshRenderer skyrender;
+public Material material2;
+public Transform esfera;
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(globalvariables.puntos>=20){
+			MeshRenderer mesh= esfera.GetComponent<MeshRenderer>();
+			mesh.material=material2;
+			mesh.materials[0]=material2;
+			mesh.materials[1]=material2;
+		}
+		if(Input.GetKeyDown(KeyCode.N)){
+			globalvariables.puntos++;
+		}
+
+		
+		
+	}
+}
